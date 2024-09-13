e16487af4dc228ca72666181c8527985b4ecf585
max-im <pogidaevmo@gmail.com>
2024-09-13 09:31:48 +0300

    update retrieving token

--- sleeper/apps/auth/src/guards/jwt-auth.guard.ts
+++ sleeper/apps/auth/src/guards/jwt-auth.guard.ts
@@ -1,17 +1,5 @@
-import { Injectable, ExecutionContext } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import { Observable } from 'rxjs';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {
-    canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
-        const req = context.switchToHttp().getRequest();
-        const token = req.cookies['Authorization'];
-
-        if (token) {
-            req.headers.authorization = `Bearer ${token}`;
-        }
-
-        return super.canActivate(context);
-    }
-}
+export class JwtAuthGuard extends AuthGuard('jwt') {}
--- sleeper/apps/auth/src/strategies/jwt.strategy.ts
+++ sleeper/apps/auth/src/strategies/jwt.strategy.ts
@@ -2,13 +2,14 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { ConfigService } from '@nestjs/config';
+import { Request } from 'express';
 import { ITokenDto } from '../dto/token.dto';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
     constructor(private readonly configService: ConfigService) {
         super({
-            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            jwtFromRequest: ExtractJwt.fromExtractors([(request: Request) => request?.cookies?.Authorization]),
             ignoreExpiration: false,
             secretOrKey: configService.get<string>('JWT_SECRET_OR_KEY'),
         });
